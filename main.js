4956b325ffe00020591114016c6aee2056cdf867
Jeetu Saini <js3924722@gmail.com>
2026-03-19 06:54:35 -1100

    Added portfolio website

---
+++ main.js
@@ -0,0 +1,42 @@
+function loadImage(id, targetId) {
+    var el = document.getElementById(id);
+    var targetEl = targetId ? document.getElementById(targetId) : el;
+    var imageToLoad;
+    
+    if (el.dataset.image) {
+        imageToLoad = el.dataset.image;
+    } else if (typeof el.currentSrc== 'underfined') {
+        imageToLoad = el.src;
+}   else {
+    imageToLoad = el.currentSrc;
+}
+
+if (imageToLoad) {
+    var img = new Image();
+    img.src = imageToLoad;
+    img.onload = function() {
+        targetEl.classList.add('is-loaded');
+    };
+ }
+}
+function loadImage(id, targetId) {}
+var el = document.getElementById(id);
+var targetEl = targetId ? document.getElementById(targeteId) : el;
+var imageToLoad;
+if (el.dataset.image) {
+    imageToLoad = el.dataset.image;
+} else if (typeof el.currentSrc === 'underefined') {
+    imageToLoad = el.src;
+} else { 
+    imageToLoad = el.currentSrc;
+}
+if (imageToLoad)
+    var img = new Image();
+img.src = imageToLoad;
+img.onload = function() {
+    targetEl.classList.add('is-loaded');
+};
+document.addEventListener('DOMContentLoaded', function() {
+    loadImage('wallpaper');
+    loadImage('pictureImage', 'picture');
+});
